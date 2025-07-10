cc537a71f43aad5fa438a6dbe15aee427b7a0a36
KevinFernandez21 <kdfernan@espol.edu.ec>
2025-07-09 22:34:08 -0500

    add screen

--- core/screen_test.py
+++ core/screen_test.py
@@ -1,23 +1,40 @@
-from xpt2046 import Touch
-from gpiozero import Button, DigitalOutputDevice
 import board
-import busio
-from time import sleep
+import displayio
+import adafruit_ili9341
+import adafruit_xpt2046
+import digitalio
+import time
 
-# touch callback
-def touchscreen_press(x, y):
-    print(x,y)
+# Configurar SPI
+spi = board.SPI()
 
-cs = DigitalOutputDevice(17)
-clk = board.SCLK_1		# same as writing 21
-mosi = board.MOSI_1	# same as writing 20
-miso = board.MISO_1	# same as writing 19
-irq = Button(26)
+# Pines del display
+tft_cs = board.CE0
+tft_dc = board.D24
+tft_reset = board.D25
 
-spi = busio.SPI(clk, mosi, miso)	# auxiliary SPI
+# Pines del touch
+touch_cs = board.CE1
+touch_irq = board.D23
 
-xpt = Touch(spi, cs=cs, int_pin=irq, int_handler=touchscreen_press)
+# Inicializar display
+displayio.release_displays()
+display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=tft_reset)
+display = adafruit_ili9341.ILI9341(display_bus, width=240, height=320)
 
+# Inicializar touch
+touch = adafruit_xpt2046.XPT2046(spi, cs=touch_cs, irq=touch_irq)
+
+# Crear grupo principal
+splash = displayio.Group()
+display.show(splash)
+
+print("Pantalla ILI9341 con touch conectada")
+
+# Loop para detectar toques
 while True:
-    #print(xpt.get_touch()) # to get the (x, y) coords when you desire
-    sleep(.01)
+    if touch.touched:
+        point = touch.touch_point
+        if point:
+            print(f"Touch detectado en: x={point[0]}, y={point[1]}")
+    time.sleep(0.1)
